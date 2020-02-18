7bfc4e6227f8a06986331e2764cc34bd7a5f1762
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-02-18 13:35:24 -0500

    automatic scroll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -583,6 +583,40 @@ function capital_letter(str) {
     // return the capitalize string put back together with the 'space' separator.
     return str.join(" ");
 };
+
+
+
+var scrollY = 0;
+var distance = 10;
+var speed =40;
+
+function autoScrollTo(el) {
+    var currentY = window.pageYOffset;
+    var targetY = document.getElementById(el).offsetTop;
+    var bodyHeight = document.body.offsetHeight;
+    var yPos = currentY + window.innerHeight;
+    var animator = setTimeout('autoScrollTo(\'' + el + '\')', speed);
+    if (yPos > bodyHeight) {
+
+        clearTimeout(animator);
+    } else {
+
+        if (currentY < targetY - distance) {
+            scrollY = currentY + distance;
+            window.scroll(0, scrollY);
+
+        }
+        else {
+
+            clearTimeout(animator);
+        }
+    }
+
+
+}
+
+
+
 // --------------------------------------------------------------------------------------
 // end of the capital_letter() function
 // --------------------------------------------------------------------------------------
