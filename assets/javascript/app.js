89fca8f8b6285c93ffb922d11b0e5ee6b83d7ebb
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-02-19 11:17:17 -0500

    links causing automatic scroll

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -584,36 +584,51 @@ function capital_letter(str) {
     return str.join(" ");
 };
 
+// jquery autoscroll
+$('a').on('click', function (e) {
 
+    if (this.hash !== '') {
+        e.preventDefault();
+        const hash = this.hash;
 
-var scrollY = 0;
-var distance = 10;
-var speed =40;
-
-function autoScrollTo(el) {
-    var currentY = window.pageYOffset;
-    var targetY = document.getElementById(el).offsetTop;
-    var bodyHeight = document.body.offsetHeight;
-    var yPos = currentY + window.innerHeight;
-    var animator = setTimeout('autoScrollTo(\'' + el + '\')', speed);
-    if (yPos > bodyHeight) {
-
-        clearTimeout(animator);
-    } else {
-
-        if (currentY < targetY - distance) {
-            scrollY = currentY + distance;
-            window.scroll(0, scrollY);
-
-        }
-        else {
-
-            clearTimeout(animator);
-        }
+        $('html, body').animate({ scrollTop: $(hash).offset().top}, 2400);
     }
+});
 
 
-}
+
+
+
+// javascript AutoScroll
+
+// var scrollY = 0;
+// var distance = 10;
+// var speed =40;
+
+// function autoScrollTo(el) {
+//     var currentY = window.pageYOffset;
+//     var targetY = document.getElementById(el).offsetTop;
+//     var bodyHeight = document.body.offsetHeight;
+//     var yPos = currentY + window.innerHeight;
+//     var animator = setTimeout('autoScrollTo(\'' + el + '\')', speed);
+//     if (yPos > bodyHeight) {
+
+//         clearTimeout(animator);
+//     } else {
+
+//         if (currentY < targetY - distance) {
+//             scrollY = currentY + distance;
+//             window.scroll(0, scrollY);
+
+//         }
+//         else {
+
+//             clearTimeout(animator);
+//         }
+//     }
+
+
+// }
 
 
 
